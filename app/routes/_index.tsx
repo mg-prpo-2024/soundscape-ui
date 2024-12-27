55b53c0c6baf04d99af70d0ba736878535616108
marko154 <marko.gartnar777@gmail.com>
2024-12-27 14:04:47 +0100

    init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,20 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "SoundScape" },
+    { name: "description", content: "Music app" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-1">
+        <h1>Hello world</h1>
+        <Button>Continue</Button>
+      </div>
+    </div>
+  );
+}
