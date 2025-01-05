a695e370ba7d917b880f321b2370522a4033c03d
marko154 <marko.gartnar777@gmail.com>
2025-01-05 19:55:01 +0100

    update payment success page

--- app/routes/subscriptions/success.tsx
+++ app/routes/subscriptions/success.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router";
+
 export default function Success() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex flex-col items-center gap-1">
-        <h1>Success</h1>
+    <div className="flex h-full items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold">Your payment was successful!</h1>
+        <Link to="/" className="text-blue-300 underline">
+          Start using Soundscape
+        </Link>
       </div>
     </div>
   );
